e18769399eaeeaf72d972d474450d316f3d8c4ef
Quentin <quentinb2004@gmail.com>
2023-06-06 13:32:50 +0200

    merge

--- src/main/java/fr/g1b/sae201/SisAppController.java
+++ src/main/java/fr/g1b/sae201/SisAppController.java
@@ -3,6 +3,8 @@ package fr.g1b.sae201;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -41,6 +43,7 @@ public class SisAppController {
     @FXML
     private Button checkBoxMenuBtn;
 
+
     // Autres
     private boolean isCheckBoxContainerVisible;
     private boolean isFilterContainerVisible;
@@ -56,9 +59,11 @@ public class SisAppController {
 
     public void initialize() {
         System.out.println("App launched successfully!");
+        ImageView view = new ImageView(new Image("menuButton.png"));
+        view.setPreserveRatio(true);
+        view.setFitWidth(30);
+        checkBoxMenuBtn.setGraphic(view);
 
-        rightMenuSize = rightMenuContainer.getPrefWidth();
-        leftMenuSize = leftMenuContainer.getPrefWidth(;
         // Initialisation de l'interface
         InterfaceInitialize();
 
