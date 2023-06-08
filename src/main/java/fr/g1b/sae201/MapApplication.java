58fbc0352f06c6fec278ef74c48539a917b58a43
Carlos <thiscomputer@here.com>
2023-06-08 18:01:06 +0200

    Ah bah finalement j'ai rien dit gluon c'est facile Il me manque plus qu'a trouver un moyen d'ajouter des points et peut-être des zones, comprendre les méthodes des coordonnées etc...

--- src/main/java/fr/g1b/sae201/MapApplication.java
+++ src/main/java/fr/g1b/sae201/MapApplication.java
@@ -1,17 +1,26 @@
 package fr.g1b.sae201;
 
 import javafx.application.Application;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import com.gluonhq.maps.*;
 
 public class MapApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
-        WebView webView = new WebView();
-        webView.getEngine().load("https://www.google.com/maps"); // Replace with your preferred map provider URL
+        //WebView webView = new WebView();
+        //webView.getEngine().load("https://giphy.com/gifs/rick-roll-g7GKcSzwQfugw"); //Ouais tant pis j'ai rien dit on peut abandonner ça
+        MapView carte = new MapView();
 
-        Scene scene = new Scene(webView, 800, 600);
+        Circle circle = new Circle(7, Color.RED);
+        circle.setCursor(Cursor.HAND);
+        circle.setVisible(true);
+
+        Scene scene = new Scene(carte, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
