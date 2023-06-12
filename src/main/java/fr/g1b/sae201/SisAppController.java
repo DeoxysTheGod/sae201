106088cf67a7c29697e2c8db16b1fe13ed6af2c0
d22002478 <mathis.delmas@etu.univ-amu.fr>
2023-06-12 16:32:49 +0200

    -correctif bug des couleur sur les textes (elles s'affichaient en blanc et était donc invisible ) - ajout d'un menu déroulent pour les régions -ajout d'un bouton clear pour les filtres

--- src/main/java/fr/g1b/sae201/SisAppController.java
+++ src/main/java/fr/g1b/sae201/SisAppController.java
@@ -121,7 +121,7 @@ public class SisAppController {
         leftMenuContainer.setPrefWidth(0.0);
         rightMenuContainer.setPrefWidth(0.0);
         regions = FXCollections.observableArrayList(
-                "Auvergne-Rhône-Alpes", "Bourgogne-Franche-Comté", "Bretagne", "Centre-Val de Loire",
+                "Auvergne-Rhône-Alpes", "Alpes Maritimes", "Bourgogne-Franche-Comté", "Bretagne", "Centre-Val de Loire",
                 "Corse", "Grand Est", "Hauts-de-France", "Île-de-France", "Normandie", "Nouvelle-Aquitaine",
                 "Occitanie", "Pays de la Loire", "Provence-Alpes-Côte d'Azur"
         );
